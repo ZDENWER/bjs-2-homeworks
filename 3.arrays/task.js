a7bc35b0e415b9ed7a77c50dafc6d0aef1e87229
ZDENWER <denwer-zelensky@mail.ru>
2022-12-31 13:07:30 +0300

    hw3-done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
-function compareArrays(arr1, arr2) {
-  
+function compareArrays(arr1, arr2){
+    return (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let resultFilter = users.filter(user => user.gender === gender);
+
+    if (users.length === 0 || resultFilter.length === 0){
+        return 0;
+    }
+
+    let resultMap = resultFilter.map(user => user.age);
+
+    return resultMap.reduce((acc, age) => acc + age) / resultMap.length;
 }
+
