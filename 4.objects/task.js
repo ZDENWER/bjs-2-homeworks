415211ac8c94206b1ff3683cd356f77876893dda
DENWER <denwer-zelensky@mail.ru>
2023-01-02 18:00:59 +0300

    hw4 60% done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
-}
+  	this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  
+ 	this.subject = subjectName
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+	if (!this.hasOwnProperty('marks')) {
+		return 0
+	};
+
+	this.marks.push(...marksToAdd)
 }
 
 Student.prototype.getAverage = function () {
-  
+  	if (!this.hasOwnProperty('marks') || this.marks.length === 0) {
+		return 0
+	};
+
+	this.marks.reduce((acc, value) => acc + value);
 }
 
 Student.prototype.exclude = function (reason) {
   
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);	
