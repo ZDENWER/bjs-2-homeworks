7368e3c4ac3f2c9fd3eb35d8f59a13716617a42f
ZDENWER <denwer-zelensky@mail.ru>
2023-01-10 07:10:31 +0300

    hw1 in work

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,10 @@
-﻿
+﻿function parseCount(value) {
+    let result = Number.parseFloat(value);
+    if (isNaN(result)) {
+        throw new Error ('Невалидное значение');
+    } else {
+        return result;
+    }
+}
+
+let value = 'Ghbdtn';
