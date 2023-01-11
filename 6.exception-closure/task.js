b17bd6d2795989a29a9fa7f08dbdb1e1fd949fe9
DENWER <denwer-zelensky@mail.ru>
2023-01-11 17:36:05 +0300

    hw 6 in work

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -7,4 +7,35 @@
     }
 }
 
-let value = 'Ghbdtn';
+function validateCount(val) {
+    try {
+        return parseCount(val);
+    } catch (error) {
+        return error
+    }
+}
+
+
+class Triangle {
+    constructor(a, b, c) {
+        
+        if (a > b + c || b > a + c || c > a + b) {
+            throw new Error ('Треугольник с такими сторонами не существует')
+        }
+           
+        this.a = a,
+        this.b = b,
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = this.perimeter * 0.5;
+        let S = (Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+        return Number(S);
+    }
+}
+
